1f8e54f909835cc45e5e1a2648b74a7b497b60da
Modar Hamdan <modar.hamdan@georgebrown.ca>
2022-11-21 14:15:58 -0500

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => res.json({ msg: "hello World" }));
+
+//connect db
+connectDB();
 
 //Define Routes
 app.use("/api/users", require("./routes/users"));
