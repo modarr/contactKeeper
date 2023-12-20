c8d7932543d8831ab03357e05d8f944e5fab8249
Modar Hamdan <modar.hamdan@georgebrown.ca>
2023-12-20 13:36:07 -0500

    express setup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/user");
 router.post("/", (req, res) => {
   res.send("register a user");
 });
