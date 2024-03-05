b0320e72803a254435b767ad9774ba1168dd384a
Modar Hamdan <modar.hamdan@georgebrown.ca>
2024-03-05 18:16:17 -0500

    update

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user");
-router.post("/", (req, res) => {
+// const User = require("../models/user");
+router.get("/", (req, res) => {
   res.send("register a user");
 });
 module.exports = router;
