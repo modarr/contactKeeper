931b1f0cbdeaedaaa93a222113b24291d61787e5
Modar Hamdan <modar.hamdan@georgebrown.ca>
2024-03-05 20:55:23 -0500

    register login auth

---
+++ routes/auth.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../middleware/auth");
+const { validationResult, check } = require("express-validator");
+
+const User = require("../models/user");
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+router.post(
+  "/",
+  [
+    check("email", "Email is invalid").isEmail(),
+    check(
+      "password",
+      "Password is required and must be min 6 characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ mesg: "invalid credintials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ mesg: "invalid credintials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {}
+    console.error(err.message);
+    return res.status(500).send({ mesg: "server error" });
+  }
+);
+
+// router.post("/", (req, res) => {
+//   res.send("login user");
+// });
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.json({ msg: "hello World" }));
+
+//connect db
+connectDB();
+
+// init middleware
+
+app.use(express.json({ extended: false }));
+
+//Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.listen(PORT, () => console.log(`server on ${PORT}`));
